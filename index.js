04804eeebee106d438b6de92e1f1bc11c3949d04
Bao Poo <baomien12@gmail.com>
2023-05-15 11:14:46 +0700

    build: develop the responsive interface and slider functionality of the service-slider section

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const menuRight = document.querySelector('.hero-header-right');
 const menuButton = document.querySelector('.hero-header_button');
 
+const sliderService = document.querySelector('.service-steps');
+const sliderServiceItem = document.querySelectorAll('.service-steps_card');
+const sliderLength = sliderServiceItem.length;
+const sliderItemWidth = 385;
+const sliderDot = document.querySelectorAll('.service-steps_pagination-item');
+console.log(sliderDot.length);
+
 let flag = false;
+let checkSliderLength = 0;
+let positionX = 0;
 
 
 const onClickToggleMenu = () => {
@@ -16,3 +25,20 @@ const onClickToggleMenu = () => {
         menuButton.classList.add('fa-bars');
     }
 }
+
+const changeSliderService = () => {
+    sliderDot[checkSliderLength].classList.remove('gradient-color');
+    checkSliderLength++;
+    if (checkSliderLength > sliderLength - 1 ) {
+        checkSliderLength = 0;
+        positionX = 0;
+    } else {
+        positionX = positionX - sliderItemWidth;
+    }
+    sliderDot[checkSliderLength].classList.add('gradient-color');
+    sliderService.style = `transform: translateX(${positionX}px)`
+}
+
+setInterval(() => {
+    changeSliderService();
+}, 1500);
